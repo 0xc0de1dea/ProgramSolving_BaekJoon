421d3bd2848c7f417c35c21562e8cc70828a49fc
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-09 22:53:34 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] argu) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        System.out.print("         ,r'\"7\n" +
+                            "r`-_   ,'  ,/\n" +
+                            " \\. \". L_r'\n" +
+                            "   `~\\/\n" +
+                            "      |\n" +
+                            "      |");
+    }
+}
