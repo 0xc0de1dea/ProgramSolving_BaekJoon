d011b39d0b21e515d4c9cbc6394005582eb433ca
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-09 22:42:54 +0900

    [Bronze V] Title: Hello World, Time: 128 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] argu) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int zz = 0;
+        for (int i = 0; i < 77777777; i++) zz++;
+        System.out.println("Hello World!");
+    }
+}
