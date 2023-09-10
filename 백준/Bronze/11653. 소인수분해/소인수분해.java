3aacf949a31e860f1e5fac0338e75c5815cc5d71
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 15:38:00 +0900

    [Bronze I] Title: 소인수분해, Time: 164 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] argu) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int x = 2;
+        
+        while (n != 1){
+            if (n % x == 0){
+                sb.append(x).append('\n');
+                n /= x;
+            }
+            else
+                x++;
+        }
+
+        System.out.println(sb);
+    }
+}
