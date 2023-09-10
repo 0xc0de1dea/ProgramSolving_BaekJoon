54058b9026e3bb7c15013211798eb92790b7eebe
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 15:58:54 +0900

    [Silver II] Title: 색종이 만들기, Time: 164 ms, Memory: 16016 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,102 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+//import java.io.FileInputStream;
+
+public class Main {
+    static int whiteCnt = 0, blueCnt = 0;
+
+    static void rec(int[][] paper, int size, int x, int y){
+        int totSum = paper[x + size - 1][y + size - 1] - paper[x + size - 1][y - 1] - paper[x - 1][y + size - 1] + paper[x - 1][y - 1];
+        
+        if (totSum == size * size) blueCnt++;
+        else if (totSum == 0) whiteCnt++;
+        else {
+            int half = size >> 1;
+            rec(paper, half, x, y);
+            rec(paper, half, x + half, y);
+            rec(paper, half, x, y + half);
+            rec(paper, half, x + half, y + half);
+        }
+    }
+
+    static public void main(String[] args) throws Exception{
+        Reader in = new Reader();
+        
+        int n = in.nextInt();
+        int[][] paper = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) for (int j = 1; j <= n; j++) paper[i][j] = in.nextInt() + paper[i - 1][j] + paper[i][j - 1] - paper[i - 1][j - 1];
+
+        rec(paper, n, 1, 1);
+        System.out.println(whiteCnt + "\n" + blueCnt);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    char nextChar() throws Exception {
+        char ch = ' ';
+        byte c;
+        while ((c = read()) <= 32);
+        do ch = (char)c;
+        while (isAlphabet(c = read()));
+        return ch;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return 96 < c && c < 123;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
