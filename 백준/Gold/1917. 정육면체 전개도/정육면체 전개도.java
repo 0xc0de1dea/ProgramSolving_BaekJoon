4b288dd6d331bf9708b6a06349dd454545cdbab0
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 16:26:11 +0900

    [Gold I] Title: 정육면체 전개도, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/1917. 정육면체 전개도/정육면체 전개도.java
@@ -0,0 +1,168 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Cube {
+    private int t, u, r, d, l, b;
+    public int passed;
+
+    public Cube(){
+        t = u = r = d = l = b = passed = 0;
+    }
+
+    public int getBottom(){
+        return b;
+    }
+
+    public void setBottom(int num){
+        b = num;
+        passed++;
+    }
+
+    public void rotatePosX(){
+        int tmp = b;
+        b = r;
+        r = t;
+        t = l;
+        l = tmp;
+    }
+
+    public void rotateNegX(){
+        int tmp = b;
+        b = l;
+        l = t;
+        t = r;
+        r = tmp;
+    }
+
+    public void rotatePosY(){
+        int tmp = b;
+        b = d;
+        d = t;
+        t = u;
+        u = tmp;
+    }
+
+    public void rotateNegY(){
+        int tmp = b;
+        b = u;
+        u = t;
+        t = d;
+        d = tmp;
+    }
+}
+
+public class Main {
+    static int[][] planarfigure;
+    static int[] dx = new int[] { 1, 0, -1, 0 };
+    static int[] dy = new int[] { 0, 1, 0, -1 };
+    static boolean[][] isVisited;
+    static boolean check;
+
+    public static void search(Cube cube, int x, int y){
+        for (int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= 6 || ny < 0 || ny >= 6) continue;
+
+            if (planarfigure[ny][nx] == 1 && !isVisited[ny][nx]){
+                isVisited[ny][nx] = true;
+
+                switch (i){
+                    case 0 :
+                        cube.rotatePosX();
+
+                        if (cube.getBottom() != 0){
+                            check = false;
+                            return;
+                        }
+
+                        cube.setBottom(planarfigure[ny][nx]);
+                        search(cube, nx, ny);
+                        cube.rotateNegX();
+                        break;
+
+                    case 1 :
+                        cube.rotatePosY();
+
+                        if (cube.getBottom() != 0){
+                            check = false;
+                            return;
+                        }
+
+                        cube.setBottom(planarfigure[ny][nx]);
+                        search(cube, nx, ny);
+                        cube.rotateNegY();
+                        break;
+
+                    case 2 :
+                        cube.rotateNegX();
+                        
+                        if (cube.getBottom() != 0){
+                            check = false;
+                            return;
+                        }
+                        
+                        cube.setBottom(planarfigure[ny][nx]);
+                        search(cube, nx, ny);
+                        cube.rotatePosX();
+                        break;
+
+                    case 3 :
+                        cube.rotateNegY();
+                        
+                        if (cube.getBottom() != 0){
+                            check = false;
+                            return;
+                        }
+                        
+                        cube.setBottom(planarfigure[ny][nx]);
+                        search(cube, nx, ny);
+                        cube.rotatePosY();
+                        break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] argu) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 3; i++){
+            Cube cube = new Cube();
+            int sx = 0, sy = 0;
+            planarfigure = new int[6][6];
+            isVisited = new boolean[6][6];
+            check = true;
+
+            for (int j = 0; j < 6; j++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+
+                for (int k = 0; k < 6; k++){
+                    planarfigure[j][k] = Integer.parseInt(st.nextToken());
+
+                    if (planarfigure[j][k] == 1){
+                        sx = k;
+                        sy = j;
+                    }
+                }
+            }
+
+            cube.setBottom(planarfigure[sy][sx]);
+            isVisited[sy][sx] = true;
+            search(cube, sx, sy);
+
+            if (cube.passed == 6){
+                sb.append("yes").append('\n');
+            }
+            else {
+                sb.append("no").append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }  
+}
