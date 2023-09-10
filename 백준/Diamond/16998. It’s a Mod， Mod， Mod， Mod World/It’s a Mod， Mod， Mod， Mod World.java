2bbcdfa0139d20f793808860e8739231a698d561
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 16:42:42 +0900

    [Diamond IV] Title: It’s a Mod, Mod, Mod, Mod World, Time: 384 ms, Memory: 23528 KB -BaekjoonHub

---
+++ 백준/Diamond/16998. It’s a Mod， Mod， Mod， Mod World/It’s a Mod， Mod， Mod， Mod World.java
@@ -0,0 +1,107 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+//import java.io.FileInputStream;
+
+public class Main {
+    static long rec(long p, long q, long n){
+        if (p == 0) return 0;
+        if (p >= q) return (n * (n + 1) >> 1) * (p / q) + rec(p % q, q, n);
+        return n * (n * p / q) + (n / q) - rec(q, p, n * p / q);
+    }
+
+    static long gcd(long a, long b){
+        while (b > 0){
+            long r = a % b;
+            a = b; b = r;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws Exception{
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        int w = in.nextInt();
+
+        while (w-- > 0){
+            long p = in.nextLong();
+            long q = in.nextLong();
+            long n = in.nextLong();
+
+            long gcd = gcd(p, q);
+
+            sb.append((n * (n + 1) >> 1) * p - q * rec(p / gcd, q / gcd, n)).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    char nextChar() throws Exception {
+        char ch = ' ';
+        byte c;
+        while ((c = read()) <= 32);
+        do ch = (char)c;
+        while (isAlphabet(c = read()));
+        return ch;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return 96 < c && c < 123;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
