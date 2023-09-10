b7f9cf9f4b8324322bb88992bf8f8e11d9a22417
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 16:11:11 +0900

    [Gold V] Title: 합분해, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] argu) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        long[][] dp = new long[k + 1][n + 1];
+        int mod = 1000000000;
+        
+        for (int i = 1; i <= k; i++)
+            dp[i][0]= 1;
+
+        for (int i = 1; i <= n; i++)
+            dp[1][i] = 1;
+
+        for (int i = 2; i <= k; i++)
+            for (int j = 1; j <= n; j++)
+                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % mod;
+
+        System.out.println(dp[k][n]);
+    }
+}
