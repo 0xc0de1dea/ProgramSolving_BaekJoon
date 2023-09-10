6dcd56aaf30017aded61bd9078d50277972b68b3
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 16:28:24 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 452 ms, Memory: 102856 KB -BaekjoonHub

---
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.java
@@ -0,0 +1,175 @@
+//import java.io.FileInputStream;
+
+class SegmentTree {
+    final int SIZE = 1 << 17;
+    final int MAX = 1234567890;
+    int[] tree = new int[SIZE << 1];
+    int[] index = new int[SIZE << 1];
+
+    public SegmentTree(){
+
+    }
+
+    public SegmentTree(int[] data){
+        int len = data.length;
+
+        for (int i = 0; i < len; i++){
+            tree[SIZE + i] = data[i];
+            index[SIZE + i] = i + 1;
+        }
+
+        for (int i = len; i < SIZE; i++){
+            tree[SIZE + i] = MAX;
+        }
+
+        optimization();
+    }
+
+    void optimization(){
+        for (int i = SIZE - 1; i > 0; i--){
+            int l = tree[i << 1];
+            int r = tree[i << 1 | 1];
+
+            if (l > r){
+                tree[i] = r;
+                index[i] = index[i << 1 | 1];
+            }
+            else {
+                tree[i] = l;
+                index[i] = index[i << 1];
+            }
+        }
+    }
+
+    public int query(int s, int e){
+        int[] t = query(s, e, 1, 0, SIZE - 1);
+        return t[1];
+    }
+
+    int[] query(int s, int e, int node, int ns, int ne){
+        int[] t = new int[2];
+
+        if (e < ns || ne < s){
+            t[0] = t[1] = MAX;
+            return t;
+        }
+
+        if (s <= ns && ne <= e){
+            t[1] = index[node];
+            t[0] = tree[node];
+            return t;
+        }
+
+        int m = ns + ne >> 1;
+        int[] l = query(s, e, node << 1, ns, m);
+        int[] r = query(s, e, node << 1 | 1, m + 1, ne);
+
+        if (l[0] > r[0]){
+            t[1] = r[1];
+            t[0] = r[0];
+            return t;
+        }
+        else if (l[0] <= r[0]){
+            t[1] = l[1];
+            t[0] = l[0];
+            return t;
+        }
+
+        return t;
+    }
+
+    public void update(int idx, int value){
+        idx += SIZE;
+        tree[idx] = value;
+
+        while (idx > 1){
+            idx >>= 1;
+            int l = tree[idx << 1];
+            int r = tree[idx << 1 | 1];
+
+            if (l > r){
+                tree[idx] = r;
+                index[idx] = index[idx << 1 | 1];
+            }
+            else {
+                tree[idx] = l;
+                index[idx] = index[idx << 1];
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        int n = in.nextInt();
+        int[] sequence = new int[n];
+
+        for (int i = 0; i < n; i++){
+            sequence[i] = in.nextInt();
+        }
+
+        int m = in.nextInt();
+        SegmentTree segTree = new SegmentTree(sequence);
+
+        for (int i = 0; i < m; i++){
+            int a = in.nextInt();
+
+            if ((a & 1) == 0){
+                int b = in.nextInt() - 1;
+                int c = in.nextInt() - 1;
+                sb.append(segTree.query(b, c)).append('\n');
+            }
+            else {
+                int b = in.nextInt() - 1;
+                int c = in.nextInt();
+                segTree.update(b, c);
+            }
+        }
+        
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);//{ if (size == -1) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
