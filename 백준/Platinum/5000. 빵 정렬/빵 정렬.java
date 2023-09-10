b876711a2426a7fcecf0cea59f10dd83f4b437bc
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 16:35:48 +0900

    [Platinum III] Title: 빵 정렬, Time: 188 ms, Memory: 17396 KB -BaekjoonHub

---
+++ 백준/Platinum/5000. 빵 정렬/빵 정렬.java
@@ -0,0 +1,114 @@
+//import java.io.FileInputStream;
+
+class FenwickTree {
+    final int SIZE;
+    long[] tree;
+
+    public FenwickTree(int size){
+        SIZE = size;
+        tree = new long[SIZE];
+    }
+
+    public void update(int idx, long value){
+        while (idx < SIZE){
+            tree[idx] += value;
+            idx += (idx & -idx);
+        }
+    }
+
+    public long sum(int idx){
+        long sum = 0;
+
+        while (idx > 0){
+            sum += tree[idx];
+            idx -= (idx & -idx);
+        }
+
+        return sum;
+    }
+}
+
+public class Main {
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        final int MAX = 1000001;
+        
+        int n = in.nextInt();
+        int[] A = new int[n + 1];
+        int[] B = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) A[i] = in.nextInt();
+        for (int i = 1; i <= n; i++) B[i] = in.nextInt();
+
+        FenwickTree fwTree = new FenwickTree(n + 1);
+        long inversionCountingA = 0;
+
+        for (int i = n; i > 0; i--){
+            inversionCountingA += fwTree.sum(A[i] - 1);
+            fwTree.update(A[i], 1L);
+        }
+
+        fwTree = new FenwickTree(n + 1);
+        long inversionCountingB = 0;
+
+        for (int i = n; i > 0; i--){
+            inversionCountingB += fwTree.sum(B[i] - 1);
+            fwTree.update(B[i], 1L);
+        }
+
+        if (((inversionCountingA % 2) ^ (inversionCountingB % 2)) == 0){
+            System.out.print("Possible");
+        }
+        else {
+            System.out.print("Impossible");
+        }
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) <= 32);
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
