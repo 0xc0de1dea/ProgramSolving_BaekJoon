3e71386485b199c045ba45c6eaf4282bb2b3c071
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 16:22:58 +0900

    [Gold II] Title: 오름세, Time: 152 ms, Memory: 15548 KB -BaekjoonHub

---
+++ 백준/Gold/3745. 오름세/오름세.java
@@ -0,0 +1,84 @@
+//import java.io.FileInputStream;
+
+public class Main {
+    public static int lowerBound(int[] arr, int s, int e, int target){
+        while (s != e){
+            int m = (s + e) / 2;
+
+            if (arr[m] < target) s = m + 1;
+            else e = m;
+        }
+        return e;
+    }
+
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = 0;
+
+        while ((n = in.nextInt()) > 0){
+
+            int[] lis = new int[n + 1];
+            int len = 0;
+
+            for (int i = 0; i < n; i++){
+                int input = in.nextInt();
+                
+                if (input > lis[len]){
+                    lis[++len] = input;
+                }
+                else {
+                    lis[lowerBound(lis, 1, len, input)] = input;
+                }
+            }
+
+            sb.append(len).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32){
+            if (size == -1) return -1;
+        }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
