7e3fc072d64dc636ff57187feb4bd8128786cf9c
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 15:43:59 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,94 @@
+import java.util.LinkedList;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws Exception {
+		Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+		int N = in.nextInt();
+		int K = in.nextInt();
+		LinkedList<Integer> list = new LinkedList<Integer>();	
+        	
+		for(int i = 1; i <= N; i++) list.add(i);
+
+		sb.append('<');
+		int index = 0;
+		
+		while(N > 1) {
+			index = (index + (K - 1)) % N;
+			
+			sb.append(list.remove(index)).append(", "); 
+			N--;
+		}
+		
+		sb.append(list.remove()).append('>');
+		System.out.print(sb);
+	}
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    char nextChar() throws Exception {
+        char ch = ' ';
+        byte c;
+        while ((c = read()) <= 32);
+        do ch = (char)c;
+        while (isAlphabet(c = read()));
+        return ch;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return 96 < c && c < 123;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
