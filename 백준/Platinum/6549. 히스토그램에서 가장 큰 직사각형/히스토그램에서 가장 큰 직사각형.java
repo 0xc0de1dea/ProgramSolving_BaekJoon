d0c8fee7101269a6669d6d6366e5e36ff8161b9e
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 16:31:20 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 196 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -0,0 +1,89 @@
+//import java.io.FileInputStream;
+
+public class Main {
+    static int[] histogram;
+
+    public static long getArea(int len){
+        int[] stack = new int[len];
+        int sz = 0;
+        int top = -1;
+
+        long maxArea = 0;
+
+        for (int i = 0; i <= len; i++){
+            while (sz > 0 && histogram[stack[top]] >= histogram[i]){
+                int h = histogram[stack[top--]];
+                long w = --sz == 0 ? i : i - 1 - stack[top];
+
+                maxArea = Math.max(maxArea, h * w);
+            }
+
+            stack[++top] = i;
+            sz++;
+        }
+
+        return maxArea;
+    }
+    
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            int n = in.nextInt();
+            histogram = new int[n + 1];
+
+            if (n == 0) break;
+
+            for (int i = 0; i < n; i++){
+                histogram[i] = in.nextInt();
+            }
+
+            long area = getArea(n);
+            sb.append(area).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);//{ if (size == -1) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
