a425ad1b55aae700a27c17b8c35d3191079307aa
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-10 16:25:26 +0900

    [Gold I] Title: 책 페이지, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Gold/1019. 책 페이지/책 페이지.java
@@ -0,0 +1,103 @@
+//import java.io.FileInputStream;
+
+public class Main {
+    public static void counting(long n, int[] cnt, int mul){
+        while (n > 0){
+            cnt[(int)n % 10] += mul;
+            n /= 10;
+        }
+    }
+
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        long m = 1;
+        long n = in.nextLong();
+        int mul = 1;
+
+        int[] cnt = new int[10];
+
+        while (true){
+            while (m % 10 != 0 && m <= n) counting(m++, cnt, mul);
+            if (m > n) break;
+            while (n % 10 != 9 && m <= n) counting(n--, cnt, mul);
+
+            long t = (n / 10 - m / 10 + 1);
+
+            for (int i = 0; i < 10; i++) cnt[i] += mul * t;
+
+            m /= 10;
+            n /= 10;
+            mul *= 10;
+        }
+
+        for (int i = 0; i < 10; i++) sb.append(cnt[i]).append(' ');
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) <= 32);
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c) {
+        return 96 < c && c < 123;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
