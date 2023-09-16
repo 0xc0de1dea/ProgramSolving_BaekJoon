01557689fa282efe974a0fd51554f2d81cf57086
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-16 16:07:55 +0900

    [Gold IV] Title: 여행 가자, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,127 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+//import java.io.FileInputStream;
+
+public class Main {
+    static int[] parent, rank;
+
+    static int find(int node){
+        if (parent[node] == node) return node;
+        return parent[node] = find(parent[node]);
+    }
+
+    static void union(int x, int y){
+        x = find(x); y = find(y);
+
+        if (x != y){
+            if (rank[x] > rank[y]) parent[y] = x;
+            else parent[x] = y;
+
+            if (rank[x] == rank[y]) rank[y]++;
+        }
+    }
+
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) parent[i] = i;
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++){
+                int connect = in.nextInt();               
+                if (connect == 1) union(i, j);
+            }
+        }
+
+        boolean flag = true;
+        int prev = in.nextInt();
+
+        for (int i = 1; i < m; i++){
+            int next = in.nextInt();
+
+            if (find(prev) != find(next)){
+                flag = false;
+                break;
+            }
+
+            prev = next;
+        }
+
+        System.out.print(flag ? "YES" : "NO");
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    char nextChar() throws Exception {
+        char ch = ' ';
+        byte c;
+        while ((c = read()) <= 32);
+        do ch = (char)c;
+        while (isAlphabet(c = read()));
+        return ch;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return 96 < c && c < 123;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
