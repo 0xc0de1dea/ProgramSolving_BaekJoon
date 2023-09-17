e7f0829ab3021bc4969c24832a824b71e4a60630
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-17 13:18:35 +0900

    [Gold II] Title: 친구 네트워크, Time: 1068 ms, Memory: 72512 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -0,0 +1,142 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+//import java.io.FileInputStream;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent, rank, counting;
+
+    static int find(int node){
+        if (parent[node] == node) return node;
+        return parent[node] = find(parent[node]);
+    }
+
+    static void union(int x, int y){
+        x = find(x); y = find(y);
+
+        if (x != y){
+            if (rank[x] > rank[y]){
+                parent[y] = x;
+                counting[x] += counting[y];
+            }
+            else {
+                parent[x] = y;
+                counting[y] += counting[x];
+            }
+
+            if (rank[x] == rank[y]) rank[y]++;
+        }
+    }
+
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        //Reader in = new Reader();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0){
+            int f = Integer.parseInt(br.readLine());
+            HashMap<String, Integer> id = new HashMap<>(f << 1);
+            parent = new int[f << 1];
+            rank = new int[f << 1];
+            counting = new int[f << 1];
+
+            for (int i = 0; i < f << 1; i++){
+                parent[i] = i;
+                counting[i] = 1;
+            }
+
+            int num = -1;
+
+            for (int i = 0; i < f; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String a = st.nextToken();
+                String b = st.nextToken();
+
+                if (!id.containsKey(a)) id.put(a, ++num);
+                if (!id.containsKey(b)) id.put(b, ++num);
+
+                union(id.get(a), id.get(b));
+
+                sb.append(counting[find(id.get(b))]).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    char nextChar() throws Exception {
+        char ch = ' ';
+        byte c;
+        while ((c = read()) <= 32);
+        do ch = (char)c;
+        while (isAlphabet(c = read()));
+        return ch;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return 96 < c && c < 123;
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
