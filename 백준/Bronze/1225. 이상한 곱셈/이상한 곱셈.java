b77de5126e8f3d359497ea8eb56d5ea135c58b23
0xc0de1dea <131027305+0xc0de1dea@users.noreply.github.com>
2023-09-30 21:22:47 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 132 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -0,0 +1,103 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        String a = in.nextString();
+        String b = in.nextString();
+        long[] countingA = new long[10];
+        long[] countingB = new long[10];
+        long ans = 0;
+
+        for (int i = a.length() - 1; i >= 0; i--) countingA[a.charAt(i) - 48]++;
+        for (int i = b.length() - 1; i >= 0; i--) countingB[b.charAt(i) - 48]++;
+        for (int i = 1; i <= 9; i++){
+            for (int j = 1; j <= 9; j++){
+                ans += countingA[i] * countingB[j] * i * j;
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) <= 32);
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32);
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        char ch = ' ';
+        byte c;
+        while ((c = read()) <= 32);
+        do ch = (char)c;
+        while (isAlphabet(c = read()));
+        return ch;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
