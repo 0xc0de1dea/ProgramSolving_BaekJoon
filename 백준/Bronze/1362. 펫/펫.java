1b247dce7a248b28fa05ab0c7b87914ddd15ffee
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2023-10-20 15:35:08 +0900

    [Bronze II] Title: 펫, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/1362. 펫/펫.java
@@ -0,0 +1,117 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int scenarioNum = 1;
+
+        while (true){
+            int o = in.nextInt();
+            int w = in.nextInt();
+            boolean isDie = false;
+
+            if (o == 0 && w == 0) break;
+            while (true){
+                char action = in.nextChar();
+                int value = in.nextInt();
+
+                if (action == '#'){
+                    if (isDie) sb.append(String.format("%d RIP\n", scenarioNum));
+                    else {
+                        if (o >> 1 < w && w < o << 1) sb.append(String.format("%d :-)\n", scenarioNum));
+                        else sb.append(String.format("%d :-(\n", scenarioNum));
+                    }
+                    
+                    scenarioNum++;
+                    break;
+                }
+
+                if (!isDie){
+                    if (action == 'E') w -= value;
+                    else if (action == 'F') w += value;
+                    if (w <= 0) isDie = true;
+                }
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) <= 32);
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32);
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) <= 32);
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
