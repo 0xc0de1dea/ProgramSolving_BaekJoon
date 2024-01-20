606472aef28e0956884f58ea3caba343478f4530
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-01-21 01:20:40 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 132 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -12,24 +12,18 @@ public class Main {
         int N = in.nextInt();
  
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < i; j++) {
-				System.out.print(" ");
-			}
-			for (int k = 0; k < (2 * N - 1) - (2 * i); k++) {
-				System.out.print("*");
-			}
-			System.out.println();
+			for (int j = 0; j < i; j++) sb.append(' ');
+			for (int k = 0; k < (2 * N - 1) - (2 * i); k++) sb.append('*');
+			sb.append('\n');
 		}
  
 		for (int i = 0; i < N - 1; i++) {
-			for (int j = 1; j < (N - 1) - i; j++) {	
-				System.out.print(" ");
-			}
-			for (int k = 0; k < 3 + 2 * i; k++) {
-				System.out.print("*");
-			}
-			System.out.println();
+			for (int j = 1; j < (N - 1) - i; j++) sb.append(' ');
+			for (int k = 0; k < 3 + 2 * i; k++)	sb.append('*');
+			sb.append('\n');
         }
+
+        System.out.print(sb);
     }
 }
 
