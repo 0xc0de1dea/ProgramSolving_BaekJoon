09acc5a2f3d65fb7c998e3136c5121958b445856
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-01 15:34:16 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 384 ms, Memory: 37632 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,62 +1,132 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
 
 public class Main {
-    static ArrayList<Integer>[] tree;
+    static ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
     static int[] parent;
 
     public static void bfs(int start){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
 
-        while (!q.isEmpty()){
-            int cur = q.poll();
-            int size = tree[cur].size();
+        while (!queue.isEmpty()){
+            int cur = queue.poll();
 
-            for (int i = 0; i < size; i++){
-                if (parent[tree[cur].get(i)] == 0){
-                    parent[tree[cur].get(i)] = cur;
-                    q.add(tree[cur].get(i));
+            for (int i = 0; i < tree.get(cur).size(); i++){
+                int nxt = tree.get(cur).get(i);
+
+                if (parent[nxt] == 0){
+                    parent[nxt] = cur;
+                    queue.add(nxt);
                 }
             }
         }
     }
 
-    public static void main(String[] argu) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        int n = in.nextInt();
 
-        int n = Integer.parseInt(br.readLine());
-        tree = new ArrayList[n + 1];
-        parent = new int[n + 1];
-        parent[1] = 1;
-        StringTokenizer st;
-        int a = 0, b = 0;
-
-        for (int i = 1; i <= n; i++){
-            tree[i] = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++){
+            tree.add(new ArrayList<>());
         }
 
-        for (int i = 1; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+        parent[1] = 1;
 
-            tree[a].add(b);
-            tree[b].add(a);
+        for (int i = 1; i < n; i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            tree.get(a).add(b);
+            tree.get(b).add(a);
         }
 
         bfs(1);
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 2; i <= n; i++){
+        for (int i = 2; i < n + 1; i++){
             sb.append(parent[i]).append('\n');
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
     }
 }
