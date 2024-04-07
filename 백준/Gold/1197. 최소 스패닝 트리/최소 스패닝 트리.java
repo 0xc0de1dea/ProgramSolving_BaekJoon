e27a10521d0b784e2a6e3193bc995ccc58e3d8fc
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-07 15:09:51 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 340 ms, Memory: 23496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,90 +1,145 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
-class Vertex {
-    public int a, b;
-    public int cost;
-
-    public Vertex(int a, int b, int cost){
-        this.a = a;
-        this.b = b;
-        this.cost = cost;
-    }
-}
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
 
 public class Main {
     static int[] parent;
     static int[] rank;
-    static PriorityQueue<Vertex> pq = new PriorityQueue<>(new Comparator<Vertex>() {
-        @Override
-        public int compare(Vertex o1, Vertex o2){
-            return o1.cost - o2.cost;
-        }
-    });
 
-    public static int find_root(int x){
-        if (parent[x] == x) return x;
+    public static int find(int a){
+        if (a == parent[a]) return a;
 
-        return parent[x] = find_root(parent[x]);
+        return parent[a] = find(parent[a]);
     }
 
-    public static void union_root(int x, int y){
-        if (x != y){
-            if (rank[x] > rank[y]) parent[y] = x;
-            else parent[x] = y;
+    public static void union(int a, int b){
+        a = find(a);
+        b = find(b);
 
-            if (rank[x] == rank[y]){
-                rank[y]++;
+        if (a != b){
+            if (rank[a] < rank[b]){
+                parent[a] = b;
+            } else {
+                parent[b] = a;
+            }
+
+            if (rank[a] == rank[b]){
+                rank[a]++;
             }
         }
     }
 
-    public static int kruskal(int v){
-        int min = 0;
-        int cnt = 1;
-
-        while (cnt < v){
-            Vertex cur = pq.poll();
-            int x = find_root(cur.a);
-            int y = find_root(cur.b);
-
-            if (x != y){
-                union_root(x, y);
-                min += cur.cost;
-                cnt++;
-            }
-        }
-
-        return min;
-    }
-
-    public static void main(String[] argu) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
+    public static int kruskal(int[][] edges, int v, int e){
+        int weight = 0;
+        Arrays.sort(edges, (x, y) -> x[2] - y[2]);
         parent = new int[v + 1];
         rank = new int[v + 1];
-        
+
         for (int i = 1; i <= v; i++){
             parent[i] = i;
         }
 
         for (int i = 0; i < e; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            pq.add(new Vertex(a, b, c));
+            if (find(edges[i][0]) != find(edges[i][1])){
+                union(edges[i][0], edges[i][1]);
+                weight += edges[i][2];
+            }
         }
 
-        int min = kruskal(v);
-        System.out.println(min);
+        return weight;
+    }
+
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        int v = in.nextInt();
+        int e = in.nextInt();
+        int[][] edges = new int[e][3];
+
+        for (int i = 0; i < e; i++){
+            edges[i][0] = in.nextInt();
+            edges[i][1] = in.nextInt();
+            edges[i][2] = in.nextInt();
+        }
+
+        int weight = kruskal(edges, v, e);
+        System.out.print(weight);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
     }
 }
