da74bed69bf4d98265c8ee06e49a571f86da4e3b
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-08 20:01:20 +0900

    [Gold I] Title: 냅색문제, Time: 240 ms, Memory: 19908 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.java
+++ 백준/Gold/1450. 냅색문제/냅색문제.java
@@ -1,64 +1,66 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Written by 0xc0de1dea
  * Email : 0xc0de1dea@gmail.com
  */
 
-//import java.io.FileInputStream;
-
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Main {
-    static int n, c;
-    static ArrayList<Long> setA = new ArrayList<>();
-    static ArrayList<Long> setB = new ArrayList<>();
+    static int c;
+    static ArrayList<Integer> setA = new ArrayList<>();
+    static ArrayList<Integer> setB = new ArrayList<>();
 
-    static void combine(long[] weight, char type , int s, int e, long sum){
+    public static void combine(int[] weight, int idx, int end, int sum, char type){
         if (sum > c) return;
-        if (s > e){
+        if (idx > end){
             if (type == 'A') setA.add(sum);
             else if (type == 'B') setB.add(sum);
             return;
         }
 
-        combine(weight, type, s + 1, e, sum);
-        combine(weight, type, s + 1, e, sum + weight[s]);
+        combine(weight, idx + 1, end, sum, type);
+        combine(weight, idx + 1, end, sum + weight[idx], type);
     }
 
-    static int upperbound(ArrayList<Long> source, long target, int s, int e){
+    public static int upperbound(int target, int s, int e){
         while (s < e){
-            int m = s + e >> 1;
+            int m = (s + e) / 2;
 
-            if (source.get(m) <= target) s = m + 1;
-            else e = m;
+            if (setA.get(m) <= target){
+                s = m + 1;
+            } else {
+                e = m;
+            }
         }
 
-        return e;
+        return s;
     }
 
-    public static void main(String[] argu) throws Exception {
-        //System.setIn(new FileInputStream("input.in"));
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
         Reader in = new Reader();
         StringBuilder sb = new StringBuilder();
 
-        n = in.nextInt();
+        int n = in.nextInt();
         c = in.nextInt();
-        long[] weight = new long[n];
+        int[] weight = new int[n];
 
-        for (int i = 0; i < n; i++) weight[i] = in.nextLong();
-
-        combine(weight, 'A', 0, n / 2 - 1, 0);
-        combine(weight, 'B', n / 2, n - 1, 0);
-        Collections.sort(setA);
-
-        long ans = 0;
-
-        for (int i = setB.size() - 1; i >= 0; i--){
-            int cnt = upperbound(setA, c - setB.get(i), 0, setA.size());
-            ans += cnt;
+        for (int i = 0; i < n; i++){
+            weight[i] = in.nextInt();
         }
 
-        System.out.print(ans);
+        combine(weight, 0, n / 2 - 1, 0, 'A');
+        combine(weight, n / 2, n - 1, 0, 'B');
+        Collections.sort(setA);
+
+        int cnt = 0;
+
+        for (int i = 0; i < setB.size(); i++){
+            cnt += upperbound(c - setB.get(i), 0, setA.size());
+        }
+
+        System.out.print(cnt);
     }
 }
 
@@ -67,20 +69,26 @@ class Reader {
     byte[] buffer = new byte[SIZE];
     int index, size;
 
-    char nextChar() throws Exception {
-        char ch = ' ';
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
         byte c;
-        while ((c = read()) <= 32);
-        do ch = (char)c;
-        while (isAlphabet(c = read()));
-        return ch;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
     }
     
     int nextInt() throws Exception {
         int n = 0;
         byte c;
         boolean isMinus = false;
-        while ((c = read()) <= 32); //{ if (size < 0) return -1; }
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
         if (c == 45) { c = read(); isMinus = true; }
         do n = (n << 3) + (n << 1) + (c & 15);
         while (isNumber(c = read()));
@@ -102,7 +110,7 @@ class Reader {
         double n = 0, div = 1;
         byte c;
         boolean isMinus = false;
-        while ((c = read()) <= 32);
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
         if (c == 45) { c = read(); isMinus = true; }
         else if (c == 46) { c = read(); }
         do n = (n * 10) + (c & 15);
@@ -116,7 +124,7 @@ class Reader {
     }
 
     boolean isAlphabet(byte c){
-        return 96 < c && c < 123;
+        return (64 < c && c < 91) || (96 < c && c < 123);
     }
 
     byte read() throws Exception {
