2c18724c326ecb658535acd14e50346b834b2b5e
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-14 21:03:02 +0900

    [Silver I] Title: 접두사, Time: 144 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/1141. 접두사/접두사.java
@@ -0,0 +1,182 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+class Node {
+    HashMap<Character, Node> child;
+    boolean isTerminal;
+
+    public Node(){
+        this.child = new HashMap<>();
+        isTerminal = false;
+    }
+}
+
+class Trie {
+    Node root;
+
+    public Trie(){
+        this.root = new Node();
+    }
+
+    public void insert(String str){
+        Node cur = this.root;
+
+        for (int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+
+            cur.child.putIfAbsent(c, new Node());
+            cur = cur.child.get(c);
+
+            if (i == str.length() - 1){
+                cur.isTerminal = true;
+                return;
+            }
+        }
+    }
+
+    public boolean search(String str){
+        Node cur = this.root;
+
+        for (int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+
+            if (cur.child.containsKey(c)){
+                cur = cur.child.get(c);
+            } else {
+                return false;
+            }
+
+            if (i == str.length() - 1){
+                if (!cur.isTerminal){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean weakSearch(String str){
+        Node cur = this.root;
+
+        for (int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+
+            if (cur.child.get(c) == null){
+                return false;
+            }
+
+            cur = cur.child.get(c);
+        }
+
+        return true;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        int n = in.nextInt();
+        String[] words = new String[n];
+        
+        for (int i = 0; i < n; i++){
+            words[i] = in.nextString();
+        }
+
+        Arrays.sort(words, Comparator.reverseOrder());
+        Trie trie = new Trie();
+
+        int cnt = 0;
+
+        for (int i = 0; i < words.length; i++){
+            boolean isExist = trie.weakSearch(words[i]);
+
+            if (!isExist) cnt++;
+
+            trie.insert(words[i]);
+        }
+
+        System.out.print(cnt);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
