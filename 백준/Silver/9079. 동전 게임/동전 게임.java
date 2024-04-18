603b4efeaf5025be02724710edb1d917aded31d7
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-18 14:13:31 +0900

    [Silver II] Title: 동전 게임, Time: 120 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Silver/9079. 동전 게임/동전 게임.java
@@ -0,0 +1,141 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+class Coin {
+    int state;
+    int cnt;
+
+    public Coin(int state, int cnt){
+        this.state = state;
+        this.cnt = cnt;
+    }
+}
+
+public class Main {
+    static int[] bitmasking = { 7, 73, 56, 146, 448, 292, 273, 84 };
+
+    public static int bfs(int coins){
+        Queue<Coin> queue= new LinkedList<>();
+        queue.add(new Coin(coins, 0));
+        boolean[] isVisited = new boolean[512];
+        isVisited[coins] = true;
+        
+        while(!queue.isEmpty()){
+            Coin cur = queue.poll();
+
+            if (cur.state == 0 || cur.state == 511){
+                return cur.cnt;
+            }
+
+            for (int i = 0; i < 8; i++){
+                int nextState = cur.state ^ bitmasking[i];
+                
+                if (!isVisited[nextState]){
+                    queue.add(new Coin(nextState, cur.cnt + 1));
+                    isVisited[nextState] = true;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        int t = in.nextInt();
+        
+        while (t-- > 0){
+            int coins = 0;
+            int digit = 0;
+
+            for (int i = 0; i < 9; i++){
+                coins |= (in.nextChar() == 'H' ? 1 : 0) << digit++;
+            }
+
+            int cnt = bfs(coins);
+            sb.append(cnt).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) <= 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
