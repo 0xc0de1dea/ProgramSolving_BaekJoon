ab327c96e6bfad7eaa07fd71395e52908c90e99c
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-23 01:11:53 +0900

    [Silver V] Title: 정보갓 영훈이, Time: 136 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Silver/14843. 정보갓 영훈이/정보갓 영훈이.java
@@ -0,0 +1,114 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        int n = in.nextInt();
+        double totalScore = 0;
+
+        while (n-- > 0){
+            double s = in.nextDouble();
+            int a = in.nextInt();
+            int t = in.nextInt();
+            int m = in.nextInt();
+
+            totalScore += s * (1 + ((double)1 / a)) * (1 + ((double)m / t)) / 4;
+        }
+
+        int p = in.nextInt();
+        double cnt = 0;
+        
+        for (int i = 0; i < p; i++){
+            if (totalScore < in.nextDouble()){
+                cnt++;
+            }
+        }
+
+        if (100 * (cnt + 1) <= 15 * (p + 1)){
+            System.out.printf("The total score of Younghoon \"The God\" is %.2f.", totalScore);
+        } else {
+            System.out.printf("The total score of Younghoon is %.2f.", totalScore);
+        }
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
