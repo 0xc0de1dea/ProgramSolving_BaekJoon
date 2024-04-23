dd8ef63fe29453b47288698765b3dad9544d30af
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-23 20:28:53 +0900

    [Silver V] Title: 3의 배수, Time: 216 ms, Memory: 19976 KB -BaekjoonHub

---
+++ 백준/Silver/1769. 3의 배수/3의 배수.java
@@ -0,0 +1,124 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        char[] number = in.nextString().toCharArray();
+        int convertNum = 0;
+        int convertCnt = 0;
+        boolean flag = false;
+
+        if (number.length == 1){
+            if ((number[0] - '0') % 3 == 0){
+                flag = true;
+            }
+        } else {
+            for (int i = 0; i < number.length; i++){
+                convertNum += number[i] - '0';
+            }
+            
+            convertCnt++;
+
+            while (convertNum >= 10){
+                int n = convertNum;
+                convertNum = 0;
+    
+                while (n > 0){
+                    convertNum += n % 10;
+                    n /= 10;
+                }
+
+                convertCnt++;
+            }
+
+            if (convertNum < 10){
+                if (convertNum % 3 == 0){
+                    flag = true;
+                }
+            }
+        }
+
+
+
+        System.out.println(convertCnt);
+        System.out.println(flag ? "YES" : "NO");
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
