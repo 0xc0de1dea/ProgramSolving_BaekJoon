36f66065c9ae1468bf60d92079b8beb63adaee23
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-23 17:59:58 +0900

    [Silver III] Title: 놀라운 문자열, Time: 160 ms, Memory: 15088 KB -BaekjoonHub

---
+++ 백준/Silver/1972. 놀라운 문자열/놀라운 문자열.java
@@ -0,0 +1,118 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        while (true){
+            char[] sentence = in.nextString().toCharArray();
+
+            if (sentence[0] == '*') break;
+
+            boolean flag = true;
+            
+            jump : for (int i = 1; i < sentence.length; i++){
+                HashMap<String, Integer> hm = new HashMap<>();
+                
+                for (int j = 0; i + j < sentence.length; j++){
+                    String str = String.valueOf(sentence[j]) + String.valueOf(sentence[i + j]);
+                    
+                    if (hm.containsKey(str)){
+                        flag = false;
+                        break jump;
+                    } else {
+                        hm.put(str, 1);
+                    }
+                }
+            }
+
+            if (flag){
+                sb.append(String.format("%s is surprising.\n", String.valueOf(sentence)));
+            } else {
+                sb.append(String.format("%s is NOT surprising.\n", String.valueOf(sentence)));
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
