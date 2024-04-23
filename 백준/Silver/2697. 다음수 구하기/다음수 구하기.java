57ecb79e61cf6eabd047d430413dcc90063d8680
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-23 16:37:15 +0900

    [Silver II] Title: 다음수 구하기, Time: 124 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Silver/2697. 다음수 구하기/다음수 구하기.java
@@ -0,0 +1,143 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static void swap(char[] number, int i, int j){
+        char tmp = number[i];
+        number[i] = number[j];
+        number[j] = tmp;
+    }
+    
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        int t = in.nextInt();
+
+        while (t-- > 0){
+            char[] number = in.nextString().toCharArray();
+            int idx = number.length - 2;
+            boolean isBiggest = true;
+
+            while (idx >= 0){
+                if (number[idx] < number[idx + 1]){
+                    for (int i = number.length - 1; i > idx; i--){
+                        if (number[idx] < number[i]){
+                            swap(number, idx, i);
+                            break;
+                        }
+                    }
+
+                    isBiggest = false;
+                    break;
+                }
+
+                idx--;
+            }
+
+            if (isBiggest){
+                sb.append("BIGGEST").append('\n');
+            } else {
+                while (++idx < number.length - 1){
+                    int min = 10;
+                    int ptr = idx;
+                    
+                    for (int i = idx; i < number.length; i++){
+                        if (min > number[i] - '0'){
+                            min = number[i] - '0';
+                            ptr = i;
+                        }
+                    }
+
+                    swap(number, idx, ptr);
+                }
+
+                for (int i = 0; i < number.length; i++){
+                    sb.append(number[i]);
+                }
+
+                sb.append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
