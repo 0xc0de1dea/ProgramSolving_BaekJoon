45d35f2fb9f1bc4d43c5a9dcfd2460357581f45f
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-23 14:42:49 +0900

    [Silver I] Title: 팔, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.java
@@ -0,0 +1,144 @@
+import java.util.Arrays;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static long pow(long a, long b){
+        long res = 1;
+
+        while (b > 0){
+            if ((b & 1) == 1) res *= a;
+
+            a *= a;
+            b >>= 1;
+        }
+
+        return res;
+    }
+
+    public static long getTopBit(long n){
+        n |= n >> 1;
+        n |= n >> 2;
+        n |= n >> 4;
+        n |= n >> 8;
+        n |= n >> 16;
+        n |= n >> 32;
+
+        n++;
+
+        return n >> 1;
+    }
+
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        long l = in.nextLong();
+        long r = in.nextLong();
+
+        long lenL = (long)Math.floor(Math.log10(l)) + 1;
+        long lenR = (long)Math.floor(Math.log10(r)) + 1;
+        long digitL = pow(10, lenL - 1);
+        long digitR = pow(10, lenR - 1);
+
+        if (lenL != lenR){
+            System.out.print(0);
+        } else {
+            int cnt = 0;
+            
+            while (digitL > 0){
+                long leftNum = l / digitL % 10;
+                long rightNum = r / digitR % 10;
+
+                if (leftNum == rightNum){
+                    if (leftNum == 8 && rightNum == 8){
+                        cnt++;
+                    }
+
+                    digitL /= 10;
+                    digitR /= 10;
+                } else {
+                    break;
+                }
+            }
+
+            System.out.print(cnt);
+        }
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
