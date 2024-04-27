0faeab82c4b6f3f55cb952eaff1e1bb394ca7779
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-04-28 03:11:03 +0900

    [Gold V] Title: ABCDE, Time: 296 ms, Memory: 25864 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,79 +1,133 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
 
 public class Main {
-    static int n, m;
-    static Queue<Integer>[] relationship;
     static boolean[] isVisited;
-    static boolean isFound;
+    static boolean flag = false;
 
-    public static void dfs(int depth, Queue<Integer> q){
-        if (depth == 5){
-            isFound = true;
+    public static void dfs(ArrayList<ArrayList<Integer>> edges, int cur, int depth, int n){
+        if (depth == n){
+            flag = true;
             return;
         }
+        
+        isVisited[cur] = true;
 
-        if (isFound) return;
-
-        while (!q.isEmpty()){
-            int nextNode = q.poll();
-            
-            if (!isVisited[nextNode]){
-                isVisited[nextNode] = true;
-                Queue<Integer> cloneQ = new LinkedList<>();
-                Iterator iter = relationship[nextNode].iterator();
-
-                while (iter.hasNext())
-                    cloneQ.add((int)iter.next());
-
-                dfs(depth + 1, cloneQ);
-                isVisited[nextNode] = false;
+        for (int nxt : edges.get(cur)){
+            if (!flag){
+                if (!isVisited[nxt]){
+                    dfs(edges, nxt, depth + 1, n);
+                    isVisited[nxt] = false;
+                }
             }
         }
     }
 
-    public static void main(String[] argu) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        relationship = new Queue[n];
-        
-        for (int i = 0; i < n; i++)
-            relationship[i] = new LinkedList<>();
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb =  new StringBuilder();
 
-        isVisited = new boolean[n];
+        int n = in.nextInt();
+        int m = in.nextInt();
+        ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+            edges.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < m; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            relationship[a].add(b);
-            relationship[b].add(a);
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            edges.get(a).add(b);
+            edges.get(b).add(a);
         }
 
         for (int i = 0; i < n; i++){
-            if (relationship[i].size() != 0){
-                isVisited[i] = true;
-                Queue<Integer> cloneQ = new LinkedList<>();
-                Iterator iter = relationship[i].iterator();
+            isVisited = new boolean[n];
 
-                while (iter.hasNext())
-                    cloneQ.add((int)iter.next());
-
-                dfs(1, cloneQ);
-                isVisited[i] = false;
+            if (!isVisited[i] && !flag){
+                dfs(edges, i, 1, 5);
             }
         }
+        
+        System.out.print(flag ? 1 : 0);
+    }
+}
 
-        if (isFound)
-            System.out.println(1);
-        else
-            System.out.println(0);
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
     }
 }
