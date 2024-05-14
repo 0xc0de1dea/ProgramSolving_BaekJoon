82e3134e989cf5cb5d86a33e81f76da29c0e555b
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-14 19:26:43 +0900

    [Gold V] Title: 피자 굽기, Time: 220 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Gold/1756. 피자 굽기/피자 굽기.java
@@ -0,0 +1,130 @@
+import java.util.Arrays;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static int lowerbound(int[] width, int left, int right, int target){
+        while (left < right){
+            int mid = left + right >> 1;
+
+            if (width[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int d = in.nextInt();
+        int n = in.nextInt();
+
+        int[] oven = new int[d];
+        int[] width = new int[d];
+
+        for (int i = 0; i < d; i++){
+            oven[i] = in.nextInt();
+        }
+
+        width[d - 1] = oven[0];
+
+        for (int i = d - 2; i >= 0; i--){
+            width[i] = Math.min(width[i + 1], oven[d - i - 1]);
+        }
+
+        int lastIdx = -1;
+
+        for (int i = 0; i < n; i++){
+            int dough = in.nextInt();
+            lastIdx = lowerbound(width, lastIdx + 1, d, dough);
+
+            if (lastIdx == d){
+                System.out.print(0);
+                return;
+            }
+        }
+
+        System.out.print(d - lastIdx);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
