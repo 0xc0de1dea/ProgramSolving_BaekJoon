bb02af37f21d4e0d4ea4b929534318c3a190e587
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-19 13:48:23 +0900

    [Gold V] Title: 순열장난, Time: 156 ms, Memory: 14968 KB -BaekjoonHub

---
+++ 백준/Gold/10597. 순열장난/순열장난.java
@@ -0,0 +1,133 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    static int n;
+    static StringBuilder ans = new StringBuilder();
+    static StringBuilder list = new StringBuilder();
+    static boolean[] isVisited;
+    static boolean flag = false;
+
+    public static boolean isValid(int num){
+        return 1 <= num && num <= n;
+    }
+
+    public static void backtracking(char[] kriii, int idx){
+        if (idx == kriii.length){
+            ans.append(list);
+            flag = true;
+            return;
+        }
+
+        if (flag) return;
+        
+        if (!isVisited[kriii[idx] - '0']){
+            isVisited[kriii[idx] - '0'] = true;
+            list.append(kriii[idx] - '0').append(' ');
+            backtracking(kriii, idx + 1);
+            list.delete(list.length() - 2, list.length());
+            isVisited[kriii[idx] - '0'] = false;
+        }
+
+        if (idx + 1 < kriii.length){
+            int num = (kriii[idx] - '0') * 10 + (kriii[idx + 1] - '0');
+
+            if (isValid(num)){
+                if (!isVisited[num]){
+                    isVisited[num] = true;
+                    list.append(num).append(' ');
+                    backtracking(kriii, idx + 2);
+                    list.delete(num < 10 ? list.length() - 2 : list.length() - 3, list.length());
+                    isVisited[num] = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        char[] kriii = in.nextString().toCharArray();
+        n = kriii.length < 10 ? kriii.length : 9 + ((kriii.length - 9) >> 1);
+        isVisited = new boolean[n + 1];
+
+        backtracking(kriii, 0);
+
+        System.out.print(ans);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
