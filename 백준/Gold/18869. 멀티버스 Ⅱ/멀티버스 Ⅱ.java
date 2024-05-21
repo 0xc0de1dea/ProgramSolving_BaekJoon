e6a7b63df0e3121db04a9901649365992383d231
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-21 23:45:19 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 1008 ms, Memory: 176216 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
@@ -1,52 +1,137 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine(), " ");
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		int[][] arr = new int[M][N];
-		List<Integer>[] list = new ArrayList[M];
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			Set<Integer> set = new HashSet<>();
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int[][] planet = new int[m][n];
 
-			for (int j = 0; j < N; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				arr[i][j] = num;
-				set.add(num);
-			}
+        for (int i = 0; i < m; i++){
+            TreeSet<Integer> clone = new TreeSet<>();
 
-			list[i] = new ArrayList<>(set);
-			Collections.sort(list[i]);
-		}
+            for (int j = 0; j < n; j++){
+                int b = in.nextInt();
+                planet[i][j] = b;
+                clone.add(b);
+            }
+            
+            HashMap<Integer, Integer> hm = new HashMap<>();
+            int idx = 0;
+            
+            for (int item : clone){
+                hm.put(item, idx++);
+            }
 
-		// 좌표 압축
-		for (int i = 0; i < M; i++)
-			for (int j = 0; j < N; j++)
-				arr[i][j] = Collections.binarySearch(list[i], arr[i][j]);
+            for (int j = 0; j < n; j++){
+                planet[i][j] = hm.get(planet[i][j]);
+            }
+        }
 
-		int count = 0;
-		for (int i = 0; i < M; i++) {
-			for (int j = i + 1; j < M; j++) {
-				if (Arrays.equals(arr[i], arr[j]))
-					count++;
-			}
-		}
+        int cnt = 0;
 
-		System.out.println(count);
-	}
+        for (int i = 0; i < m - 1; i++){
+            for (int j = i + 1; j < m; j++){
+                boolean flag = true;
+
+                for (int k = 0; k < n; k++){
+                    if (planet[i][k] != planet[j][k]){
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag){
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
 }
