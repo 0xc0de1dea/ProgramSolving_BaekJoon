81b22fadbd1891651ac0b39f4556ec4cf15d4ae3
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-22 16:34:09 +0900

    [Gold V] Title: Ezreal 여눈부터 가네 ㅈㅈ, Time: 144 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Gold/20500. Ezreal 여눈부터 가네 ㅈㅈ/Ezreal 여눈부터 가네 ㅈㅈ.java
@@ -0,0 +1,181 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+class SegmentTree {
+    long[] arr;
+    int size;
+
+    public SegmentTree(long[] data){
+        size = (int)Math.ceil(Math.log10(data.length) / Math.log10(2));
+        arr = new long[size << 1];
+
+        for (int i = 0; i < data.length; i++){
+            arr[size + i] = data[i];
+        }
+        
+        construct();
+    }
+
+    void construct(){
+        for (int i = size - 1; i > 0; i--){
+            arr[i] = arr[i << 1] + arr[i << 1 | 1];
+        }
+    }
+
+    public long sum(int s, int e){
+        return sum(s, e, 1, 0, arr.length - 1);
+    }
+
+    long sum(int s, int e, int node, int ns, int ne){
+        if (e < ns || ne < s) return 0;
+        if (s <= ns && ne <= e) return arr[node];
+
+        int m = ns + ne >> 1;
+
+        return sum(s, e, node >> 1, ns, m) + sum(s, e, node >> 1 | 1, m + 1, ne);
+    }
+
+    public void update(int idx, long val){
+        idx += size;
+        arr[idx] = val;
+
+        while (idx > 1){
+            idx >>= 1;
+            arr[idx] = arr[idx >> 1] + arr[idx >> 1 | 1];
+        }
+    }
+}
+
+public class Main {
+    static final long MOD = 1_000_000_007;
+
+    public static long pow(long x, long y){
+        long res = 1;
+
+        while (y > 0){
+            if ((y & 1) == 1) res = (res * x) % MOD;
+
+            x = (x * x) % MOD;
+            y >>= 1;
+        }
+
+        return res;
+    }
+
+    public static long nCr(int n, int r){
+        if (n == r) return 1;
+        if (r == 1) return n;
+
+        long up = 1;
+
+        for (int i = n; i > n - r; i--){
+            up *= i;
+            up %= MOD;
+        }
+
+        long dn = 1;
+
+        for (int i = 1; i <= r; i++){
+            dn *= i;
+            dn %= MOD;
+        }
+
+        long res = (up * pow(dn, MOD - 2)) % MOD;
+
+        return res;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        long cnt = 0;
+        
+        for (int i = 0; i < n; i++){
+            int one = n - i - 1;
+            int five = 1 + i;
+
+            if ((5 * five + one) % 3 == 0){
+                cnt += nCr(n - 1, five - 1);
+                cnt %= MOD;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
