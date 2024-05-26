93b8f1f0842ecd4377f152fd7c7e5a006748e176
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-26 17:12:07 +0900

    [Gold V] Title: 동전 2, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,149 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    static final int MOD = 10_007;
+    static int[][] dp;
+
+    public static int nCr(int n, int r){
+        if (dp[n][r] > 0){
+            return dp[n][r];
+        }
+
+        if (r == 0 || r == n){
+            return dp[n][r] = 1;
+        }
+
+        return dp[n][r] = (nCr(n - 1, r - 1) + nCr(n - 1, r)) % MOD;
+    }
+
+    public static int pow(int x, int exp){
+        int res = 1;
+
+        while (exp > 0){
+            if ((exp & 1) == 1) res = (res * x) % MOD;
+
+            x = (x * x) % MOD;
+            exp >>= 1;
+        }
+
+        return res;
+    }
+
+    public static int nCrFast(int n , int r){
+        int up = 1;
+
+        for (int i = n; i > n - r; i--){
+            up *= i;
+            up %= MOD;
+        }
+
+        int dn = 1;
+
+        for (int i = 1; i <= r; i++){
+            dn *= i;
+            dn %= MOD;
+        }
+
+        return (up * pow(dn, MOD - 2)) % MOD;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] dp = new int[k + 1];
+
+        final int INF = 123456789;
+        
+        for (int i = 1; i <= k; i++){
+            dp[i] = INF;
+        }
+
+        for (int i = 0; i < n; i++){
+            int coin = in.nextInt();
+
+            for (int j = coin; j <= k; j++){
+                dp[j] = Math.min(dp[j], dp[j - coin] + 1);
+            }
+        }
+
+        System.out.print(dp[k] == INF ? -1 : dp[k]);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
