b78b8cf5c97cb0725d4104157d2c11dbfbe8dbf7
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-28 15:58:36 +0900

    [Gold IV] Title: 팰린드롬?, Time: 488 ms, Memory: 41660 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,157 @@
+import java.util.Arrays;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static int lowerbound(int[] arr, int left, int right, int target){
+        while (left < right){
+            int mid = left + right >> 1;
+
+            if (arr[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        int[] seq = new int[n + 1];
+
+        for (int i = 1; i <= n; i++){
+            seq[i] = in.nextInt();
+        }
+
+        int[][] dp = new int[n + 1][n + 1];
+        
+        for (int i = 1; i <= n; i++){
+            int left = i;
+            int right = i;
+            boolean flag = true;
+
+            while (1 <= left && right <= n){
+                if (seq[left] == seq[right]){
+                    if (flag){
+                        dp[left][right] = 1;
+                    }
+                } else {
+                    flag = false;
+                }
+
+                left--;
+                right++;
+            }
+        }
+
+        for (int i = 1; i < n; i++){
+            int left = i;
+            int right = i + 1;
+            boolean flag = true;
+
+            while (1 <= left && right <= n){
+                if (seq[left] == seq[right]){
+                    if (flag){
+                        dp[left][right] = 1;
+                    }
+                } else {
+                    flag = false;
+                }
+
+                left--;
+                right++;
+            }
+        }
+
+        int m = in.nextInt();
+
+        for (int i = 0; i < m; i++){
+            int s = in.nextInt();
+            int e = in.nextInt();
+
+            sb.append(dp[s][e]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
