6d07e813fff2c70d5afd691ea783c471f2b069d8
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-28 23:04:13 +0900

    [Gold II] Title: 사전, Time: 124 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Gold/1256. 사전/사전.java
@@ -0,0 +1,131 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        long[][] dp = new long[201][201];
+        final long INF = 0xDEADBEEFL;
+        
+        for (int i = 0; i <= 200; i++){
+            for (int j = 0; j <= 200; j++){
+                if (j > i) break;
+
+                if (j == 0 || j == i) dp[i][j] = 1;
+                else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+
+                if (dp[i][j] > INF) dp[i][j] = INF;
+            }
+        }
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        long k = in.nextLong();
+
+        int a = n;
+        int z = m;
+        char[] arr = new char[n + m];
+
+        for (int i = 0; i < n + m; i++){
+            if (a == 0){
+                arr[i] = 'z';
+                continue;
+            }
+
+            if (dp[n + m - 1 - i][a - 1] >= k){
+                arr[i] = 'a';
+                a--;
+            } else {
+                arr[i] = 'z';
+                z--;
+                k -= dp[n + m - 1 - i][a - 1];
+            }
+        }
+
+        if (a > 0){
+            sb.append(-1);
+        } else {
+            for (int i = 0; i < n + m; i++){
+                sb.append(arr[i]);
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
