5f3a64d1daffb5cbe72c3a557d21f1f538046db3
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-29 20:03:12 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1428 ms, Memory: 167408 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -0,0 +1,122 @@
+import java.util.ArrayList;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
+    static boolean[] isVisited;
+    static int[][] dp;
+
+    public static void dfs(int cur){
+        isVisited[cur] = true;
+        dp[cur][1] = 1;
+
+        for (int nxt : edges.get(cur)){
+            if (!isVisited[nxt]){
+                dfs(nxt);
+                dp[cur][0] += dp[nxt][1];
+                dp[cur][1] += Math.min(dp[nxt][0], dp[nxt][1]);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        isVisited = new boolean[n + 1];
+        dp = new int[n + 1][2];
+
+        for (int i = 0; i <= n; i++){
+            edges.add(new ArrayList<>());
+        }
+
+        for (int i = 1; i < n; i++){
+            int u = in.nextInt();
+            int v = in.nextInt();
+
+            edges.get(u).add(v);
+            edges.get(v).add(u);
+        }
+
+        dfs(1);
+
+        System.out.print(Math.min(dp[1][0], dp[1][1]));
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
