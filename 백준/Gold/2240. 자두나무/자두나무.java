6e086d89ad0cccdab17925665e8722236e1def15
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-31 00:41:36 +0900

    [Gold V] Title: 자두나무, Time: 120 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -0,0 +1,135 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    static int[][] dp;
+
+    public static int dp(int n, int m, int r, int c){
+        if (dp[r][c] > 0){
+            return dp[r][c];
+        }
+
+        if (r == n && c == m){
+            return dp[r][c] = 1;
+        }
+
+        int sum = 0;
+        
+        if (r + 1 <= n){
+            sum += dp(n, m, r + 1, c);
+        }
+
+        if (c + 1 <= m){
+            sum += dp(n, m, r, c + 1);
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int t = in.nextInt();
+        int w = in.nextInt();
+        int[][] dp = new int[t + 1][w + 1];
+
+        for (int i = 1; i <= t; i++){
+            int loc = in.nextInt();
+
+            for (int j = Math.min(i, w); j >= 0; j--){
+                dp[i][j] = dp[i - 1][j];
+
+                if (j % 2 != loc % 2){
+                    dp[i][j]++;
+                }
+            }
+
+            for (int j = Math.min(i, w); j > 0; j--){
+                dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
+            }
+        }
+
+        int max = 0;
+
+        for (int i = 0; i <= w; i++){
+            max = Math.max(max, dp[t][i]);
+        }
+
+        System.out.print(max);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
