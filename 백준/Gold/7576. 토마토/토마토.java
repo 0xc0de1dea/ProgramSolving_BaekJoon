1e20f820cbe42435ce0b2ab6afe71d3cb0392bd6
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-31 15:44:12 +0900

    [Gold V] Title: 토마토, Time: 380 ms, Memory: 59708 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,87 +1,146 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
 
 public class Main {
-    static int m, n;
-    static int[][] box;
-    static boolean[][] isVisited;
-    static int[] dirR = new int[] { 0, 1, 0, -1 };
-    static int[] dirC = new int[] { 1, 0, -1, 0 };
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
 
-    public static int bfs(){
-        Queue<int[]> q = new LinkedList<>();
-        int day = -1;
+        int[] dimension = new int[2];
+        int size = 1;
 
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (box[i][j] == 1){
-                    q.add(new int[] { i, j });
-                    isVisited[i][j] = true;
+        for (int i = 0; i < 2; i++){
+            dimension[i] = in.nextInt();
+            size *= dimension[i];
+        }
+
+        int[] map = new int[size];
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < size / dimension[0]; i++){
+            for (int j = 0; j < dimension[0]; j++){
+                map[i * dimension[0] + j] = in.nextInt();
+
+                if (map[i * dimension[0] + j] == 1){
+                    queue.add(i * dimension[0] + j);
                 }
             }
         }
 
-        while (q.size() != 0){
-            Queue<int[]> q2 = new LinkedList<>();
+        while (!queue.isEmpty()){
+            int cur = queue.poll();
+            int k = 1;
+            int d = 1;
 
-            while (q.size() != 0){
-                q2.add(q.remove());
-            }
+            for (int i = 0; i < 2; i++){
+                d *= dimension[i];
 
-            while(q2.size() != 0){
-                int[] curQ = q2.remove();
-                int curR = curQ[0];
-                int curC = curQ[1];
+                for (int j = -1; j <= 1; j += 2){
+                    int nxt = cur + j * k;
 
-                for (int i = 0; i < 4; i++){
-                    int nextR = curR + dirR[i];
-                    int nextC = curC + dirC[i];
-
-                    if (nextR < 0 || nextR >= n || nextC < 0 || nextC >= m)
-                        continue;
-
-                    if (box[nextR][nextC] != -1 && !isVisited[nextR][nextC]){
-                        q.add(new int[] { nextR, nextC });
-                        isVisited[nextR][nextC] = true;
+                    if (0 <= nxt && nxt < size){
+                        if (nxt / d == cur / d){
+                            if (map[nxt] == 0){
+                                queue.add(nxt);
+                                map[nxt] = map[cur] + 1;
+                            }
+                        }
                     }
                 }
-            }
 
-            day++;
-        }
-
-        jump : for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (!isVisited[i][j] && box[i][j] != -1){
-                    day = -1;
-                    break jump;
-                }
+                k *= dimension[i];
             }
         }
-                    
-        return day;
+
+        int time = 0;
+        boolean flag = true;
+
+        for (int i = 0; i < size; i++){
+            time = Math.max(time, map[i] - 1);
+
+            if (map[i] == 0){
+                flag = false;
+                break;
+            }
+        }
+
+        System.out.print(flag ? time : -1);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
     }
 
-    public static void main(String[] argu) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        box = new int[n][m];
-        isVisited = new boolean[n][m];
-        
-        for (int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
 
-            for (int j = 0; j < m; j++)
-                box[i][j] = Integer.parseInt(st.nextToken());
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
         }
-
-        System.out.println(bfs());
+        return buffer[index++];
     }
 }
