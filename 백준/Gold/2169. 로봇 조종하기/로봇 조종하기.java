c638808e1d189bd83eed6940e22556c7b2c9954c
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-05-31 21:04:23 +0900

    [Gold II] Title: 로봇 조종하기, Time: 432 ms, Memory: 63020 KB -BaekjoonHub

---
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -0,0 +1,139 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    static int n, m;
+    static int[][] map;
+    static int[][][] dp;
+    static boolean[][] isVisited;
+    static final int INF = 123456789;
+
+    public static int find(int r, int c, int type){
+        if (dp[r][c][type] != -INF){
+            return dp[r][c][type];
+        }
+
+        if (r == n && c == m){
+            return dp[r][c][type] = map[r][c];
+        }
+
+        int max = -INF;
+        isVisited[r][c] = true;
+
+        if (r + 1 <= n){
+            if (!isVisited[r + 1][c]){
+                max = Math.max(max, find(r + 1, c, 0));
+            }
+        }
+
+        if (c - 1 > 0){
+            if (!isVisited[r][c - 1]){
+                max = Math.max(max, find(r, c - 1, 1));
+            }
+        }
+
+        if (c + 1 <= m){
+            if (!isVisited[r][c + 1]){
+                max = Math.max(max, find(r, c + 1, 2));
+            }
+        }
+
+        isVisited[r][c] = false;
+
+        return dp[r][c][type] = map[r][c] + max;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        n = in.nextInt();
+        m = in.nextInt();
+        map = new int[n + 1][m + 1];
+        dp = new int[n + 1][m + 1][3];
+        isVisited = new boolean[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){
+                map[i][j] = in.nextInt();
+                dp[i][j][0] = dp[i][j][1] = dp[i][j][2] = -INF;
+            }
+        }
+
+        System.out.println(find(1, 1, 0));
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
