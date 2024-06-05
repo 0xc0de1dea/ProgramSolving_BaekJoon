af690286a13b1b2a40149d089569f247e6b5d598
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-06-06 08:59:20 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 160 ms, Memory: 22116 KB -BaekjoonHub

--- 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.java
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.java
@@ -42,8 +42,16 @@ import java.util.Arrays;
         n = in.nextInt();
         m = in.nextInt();
         dp = new long[n + 1][m + 1];
+        dp[1][1] = 1;
 
-        System.out.println(dp(1, 1));
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){
+                dp[i][j] += dp[i - 1][j] + dp[i][j - 1] + dp[i - 1][j - 1];
+                dp[i][j] %= MOD;
+            }
+        }
+
+        System.out.println(dp[n][m]);
     }
 }
 
