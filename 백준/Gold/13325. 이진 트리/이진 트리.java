3131291c725ae8c751a4cf13e0dcda9078f82f3a
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-06-08 01:55:01 +0900

    [Gold III] Title: 이진 트리, Time: 220 ms, Memory: 31532 KB -BaekjoonHub

---
+++ 백준/Gold/13325. 이진 트리/이진 트리.java
@@ -0,0 +1,134 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    static int leaf;
+    static int[] tree;
+    static int[] dp;
+
+    public static int pow(int a, int b){
+        int res = 1;
+
+        while (b > 0){
+            if ((b & 1) == 1) res *= a;
+
+            a *= a;
+            b >>= 1;
+        }
+
+        return res;
+    }
+
+    public static int dfs(int cur){
+        if (cur >= leaf) return tree[cur];
+
+        return tree[cur] + Math.max(dfs(cur << 1), dfs(cur << 1 | 1));
+    }
+
+    public static int dp(int cur){
+        if (cur >= leaf) return dp[cur] = tree[cur];
+
+        int left = dp(cur << 1);
+        int right = dp(cur << 1 | 1);
+        int diff = Math.abs(left - right);
+
+        dp[cur] = tree[cur] + dp[cur << 1] + dp[cur << 1 | 1] + diff;
+
+        return tree[cur] + Math.max(left, right);
+    }
+
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+        
+        int k = in.nextInt();
+        leaf = pow(2, k);
+        int size = pow(2, k + 1);
+        tree = new int[size];
+        dp = new int[size];
+        
+        for (int i = 2; i < size; i++){
+            tree[i] = in.nextInt();
+        }
+
+        int max = dfs(1);
+        dp(1);
+
+        System.out.println(dp[1]);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
