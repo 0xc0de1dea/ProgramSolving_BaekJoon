9fc74255780ed47a702934bee8c6c8ed2039e9d1
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-06-09 14:32:41 +0900

    [Silver V] Title: 네모네모 시력검사, Time: 108 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/18242. 네모네모 시력검사/네모네모 시력검사.java
@@ -0,0 +1,153 @@
+import java.util.ArrayList;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    static char[][] map;
+    static ArrayList<int[]> list = new ArrayList<>();
+    static boolean[][] isVisited;
+    static int[] dr = { 0, 1, 0, -1 };
+    static int[] dc = { 1, 0, -1, 0 };
+
+    public static void dfs(int r, int c){
+        isVisited[r][c] = true;
+
+        boolean flag = false;
+
+        for (int i = 0; i < 4; i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if (map[nr][nc] == '#'){
+                if (!isVisited[nr][nc]){
+                    dfs(nr, nc);
+                    flag = true;
+                }
+            }
+        }
+
+        if (!flag){
+            list.add(new int[] { r, c });
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        map = new char[n + 2][m + 2];
+        isVisited = new boolean[n + 1][m + 1];
+        
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){
+                map[i][j] = in.nextChar();
+            }
+        }
+
+        jump : 
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){
+                if (map[i][j] == '#'){
+                    dfs(i, j);
+                    
+                    int dy = Math.abs(list.get(0)[0] - list.get(1)[0]);
+                    int dx = Math.abs(list.get(0)[1] - list.get(1)[1]);
+
+                    if (dy == 0){
+                        if (i == list.get(0)[0]){
+                            System.out.println("UP");
+                        } else {
+                            System.out.println("DOWN");
+                        }
+                    } else if (dx == 0){
+                        if (j == list.get(0)[1]){
+                            System.out.println("LEFT");
+                        } else {
+                            System.out.println("RIGHT");
+                        }
+                    }
+
+                    break jump;
+                }
+            }
+        }
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
