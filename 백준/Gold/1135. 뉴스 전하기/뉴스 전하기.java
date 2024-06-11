7f30deb8821349abca158d17c46981829ff6af86
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-06-11 22:03:51 +0900

    [Gold II] Title: 뉴스 전하기, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.java
@@ -0,0 +1,124 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
+
+    public static int dfs(int cur){
+        if (edges.get(cur).isEmpty()) return 0;
+
+        ArrayList<Integer> child = new ArrayList<>();
+
+        for (int nxt : edges.get(cur)){
+            child.add(dfs(nxt));
+        }
+
+        child.sort(Comparator.reverseOrder());
+
+        int max = 0;
+
+        for (int i = 0; i < child.size(); i++){
+            max = Math.max(max, child.get(i) + (i + 1));
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+
+        for (int i = 0; i < n; i++){
+            edges.add(new ArrayList<>());
+        }
+
+        in.nextInt();
+
+        for (int i = 1; i < n; i++){
+            int parent = in.nextInt();
+            edges.get(parent).add(i);
+        }
+
+        System.out.println(dfs(0));
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
