ecd4e7797d46a5237b9237a067f93ad961b664f0
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-06-19 00:41:30 +0900

    [Silver I] Title: 동물원, Time: 124 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -10,16 +10,13 @@ public class Main {
 
         final int MOD = 9901;
         int n = in.nextInt();
-        int[][] dp = new int[3][n + 1];
-        dp[0][0] = 1;
-        
-        for (int i = 1; i <= n; i++){
-            dp[0][i] = (dp[0][i - 1] + dp[1][i - 1] + dp[2][i - 1]) % MOD;
-            dp[1][i] = (dp[0][i - 1] + dp[2][i - 1]) % MOD;
-            dp[2][i] = (dp[0][i - 1] + dp[1][i - 1]) % MOD;
-        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 3;
 
-        System.out.print((dp[0][n] + dp[1][n] + dp[2][n]) % MOD);
+        for (int i = 2; i <= n; i++) dp[i] = (dp[i - 2] + (dp[i - 1] << 1)) % MOD;
+
+        System.out.print(dp[n]);
     }
 }
 
