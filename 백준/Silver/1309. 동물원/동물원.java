275368e203adb8466634c78b8cba86188b7a81b2
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-06-18 23:49:59 +0900

    [Silver I] Title: 동물원, Time: 132 ms, Memory: 15360 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,21 +1,96 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
 
-public class Main{
-    public static void main(String[] argu) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] dp = new int[n + 1][3];
-        dp[1][0] = dp[1][1] = dp[1][2] = 1;
-        int mod = 9901;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 2; i <= n; i++){
-            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % mod;
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
-            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
+        final int MOD = 9901;
+        int n = in.nextInt();
+        int[][] dp = new int[3][n + 1];
+        dp[0][0] = 1;
+        
+        for (int i = 1; i <= n; i++){
+            dp[0][i] = (dp[0][i - 1] + dp[1][i - 1] + dp[2][i - 1]) % MOD;
+            dp[1][i] = (dp[0][i - 1] + dp[2][i - 1]) % MOD;
+            dp[2][i] = (dp[0][i - 1] + dp[1][i - 1]) % MOD;
         }
 
-        System.out.println((dp[n][0] + dp[n][1] + dp[n][2]) % mod);
+        System.out.print((dp[0][n] + dp[1][n] + dp[2][n]) % MOD);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) <= 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
     }
 }
