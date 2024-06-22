ea38fc4e276b6710e8298edcab5624c26fa993ed
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-06-22 15:42:13 +0900

    [Gold V] Title: 전구 상태 뒤집기, Time: 188 ms, Memory: 17488 KB -BaekjoonHub

---
+++ 백준/Gold/25634. 전구 상태 뒤집기/전구 상태 뒤집기.java
@@ -0,0 +1,121 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        int[] bulb = new int[n + 1];
+        int[] sumOn = new int[n + 1];
+        int[] sumOff = new int[n + 1];
+
+        for (int i = 1; i <= n; i++){
+            bulb[i] = in.nextInt();
+        }
+
+        for (int i = 1; i <= n; i++){
+            int state = in.nextInt();
+
+            if (state == 1){
+                sumOn[i] = sumOn[i - 1] + bulb[i];
+                sumOff[i] = sumOff[i - 1];
+            } else if (state == 0){
+                sumOn[i] = sumOn[i - 1];
+                sumOff[i] = sumOff[i - 1] + bulb[i];
+            }
+        }
+
+        int max = 0;
+        int prev = 1;
+
+        for (int i = 1; i <= n; i++){
+            int case1 = (sumOn[n] - sumOn[i]) + (sumOff[i] - sumOff[prev - 1]) + sumOn[prev - 1];
+            int case2 = (sumOn[n] - sumOn[i]) + (sumOff[i] - sumOff[i - 1]) + sumOn[i - 1];
+
+            if (case1 < case2){
+                prev = i;
+            }
+
+            max = Math.max(max, Math.max(case1, case2));
+        }
+
+        System.out.println(max);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) <= 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
