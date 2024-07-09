78bd15e9d84ebcbda92f0b759afc56cab5f9c403
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-07-09 21:41:25 +0900

    [Ruby V] Title: 블록, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Ruby/10050. 블록/블록.java
@@ -0,0 +1,130 @@
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    static StringBuilder progress = new StringBuilder();
+
+    public static void rec(int n, int offset){
+        if (n == 3){
+            progress.append(2).append(" to ").append(-1).append('\n');
+            progress.append(5).append(" to ").append(2).append('\n');
+            progress.append(3).append(" to ").append(-3);
+        } else if (n == 4){
+            progress.append(6 + offset).append(" to ").append(-1 + offset).append('\n');
+            progress.append(3 + offset).append(" to ").append(6 + offset).append('\n');
+            progress.append(0 + offset).append(" to ").append(3 + offset).append('\n');
+            progress.append(7 + offset).append(" to ").append(0 + offset).append('\n');
+        } else if (n == 5){
+            progress.append(8 + offset).append(" to ").append(-1 + offset).append('\n');
+            progress.append(3 + offset).append(" to ").append(8 + offset).append('\n');
+            progress.append(6 + offset).append(" to ").append(3 + offset).append('\n');
+            progress.append(0 + offset).append(" to ").append(6 + offset).append('\n');
+            progress.append(9 + offset).append(" to ").append(0 + offset).append('\n');
+        } else if (n == 6){
+            progress.append(10 + offset).append(" to ").append(-1 + offset).append('\n');
+            progress.append(7 + offset).append(" to ").append(10 + offset).append('\n');
+            progress.append(2 + offset).append(" to ").append(7 + offset).append('\n');
+            progress.append(6 + offset).append(" to ").append(2 + offset).append('\n');
+            progress.append(0 + offset).append(" to ").append(6 + offset).append('\n');
+            progress.append(11 + offset).append(" to ").append(0 + offset).append('\n');
+        } else if (n == 7){
+            progress.append(8 + offset).append(" to ").append(-1 + offset).append('\n');
+            progress.append(5 + offset).append(" to ").append(8 + offset).append('\n');
+            progress.append(12 + offset).append(" to ").append(5 + offset).append('\n');
+            progress.append(3 + offset).append(" to ").append(12 + offset).append('\n');
+            progress.append(9 + offset).append(" to ").append(3 + offset).append('\n');
+            progress.append(0 + offset).append(" to ").append(9 + offset).append('\n');
+            progress.append(13 + offset).append(" to ").append(0 + offset).append('\n');
+        } else {
+            progress.append((n << 1) - 2 + offset).append(" to ").append(-1 + offset).append('\n');
+            progress.append(3 + offset).append(" to ").append((n << 1) - 2 + offset).append('\n');
+            rec(n - 4, offset + 4);
+            progress.append(0 + offset).append(" to ").append((n << 1) - 5 + offset).append('\n');
+            progress.append((n << 1) - 1 + offset).append(" to ").append(0 + offset).append('\n');
+        }
+    }
+
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        rec(n, 0);
+        System.out.print(progress);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) > 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
