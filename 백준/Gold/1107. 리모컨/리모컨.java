acc518ecc4c74e51e0c1f405bb48b63e994a96ee
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-07-17 00:14:13 +0900

    [Gold V] Title: 리모컨, Time: 144 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,53 +1,128 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
 
-public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        StringTokenizer st = null;
-        
-        if (m != 0)
-            st = new StringTokenizer(br.readLine());
-        boolean[] brokenButton = new boolean[10];
+class Main {
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < m; i ++){
-            int tmp = Integer.parseInt(st.nextToken());
-            brokenButton[tmp] = true;
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] broken = new int[10];
+
+        for (int i = 0; i < m; i++){
+            broken[in.nextInt()] = 1;
         }
 
-        int min = Integer.MAX_VALUE;
+        int min = 0x7f7f7f7f;
 
-        for (int i = 0; i < 1000000; i++){
-            int onlyPlusMinus = Math.abs(n - 100);
-            int numButton = 0;
-            int plusMinusButton = 0xFFFFF;
-            int copy_i = i;
+        for (int i = 0; i <= 1_000_000; i++){
+            int copy = i;
+            int cnt = 0;
+            boolean flag = false;
 
-            if (copy_i == 0){
-                if (!brokenButton[0])
-                    numButton = 1;
-                else
-                    numButton = 0xFFFFF;
+            if (copy == 0){
+                if (broken[copy] == 1){
+                    flag = true;
+                }
+
+                cnt++;
             }
-            while (copy_i > 0){
-                if (brokenButton[copy_i % 10]){
-                    numButton = 0xFFFFF;
+
+            while (copy > 0){
+                if (broken[copy % 10] == 1){
+                    flag = true;
                     break;
                 }
-                copy_i /= 10;
-                numButton += 1;
+
+                copy /= 10;
+                cnt++;
             }
-            if (numButton != 0xFFFFF)
-                plusMinusButton = Math.abs(n - i);
-            
-            min = Math.min(min, Math.min(onlyPlusMinus, numButton + plusMinusButton));
+
+            cnt += Math.abs(n - i);
+
+            if (!flag){
+                min = Math.min(min, Math.min(Math.abs(n - 100), cnt));
+            } else {
+                min = Math.min(min, Math.abs(n - 100));
+            }
         }
 
         System.out.println(min);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
     }
 }
