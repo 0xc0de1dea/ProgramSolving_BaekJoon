4839f374fb10632090af401cc52146f7cd134272
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-07-26 16:33:48 +0900

    [Gold IV] Title: 도서관, Time: 128 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Gold/1461. 도서관/도서관.java
@@ -0,0 +1,129 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+public class Main {
+    public static void main(String[] argu) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        PriorityQueue<Integer> plus = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> minus = new PriorityQueue<>(Comparator.reverseOrder());
+        plus.add(0);
+        minus.add(0);
+
+        for (int i = 0; i < n; i++){
+            int book = in.nextInt();
+            
+            if (book > 0) plus.add(book);
+            else if (book < 0) minus.add(-book);
+        }
+
+        int maxVal = Math.max(plus.peek(), minus.peek());
+
+        int step = 0;
+
+        while (!plus.isEmpty()){
+            step += plus.poll() << 1;
+
+            for (int i = 1; i < m; i++){
+                if (!plus.isEmpty()){
+                    plus.poll();
+                }
+            }
+        }
+
+        while (!minus.isEmpty()){
+            step += minus.poll() << 1;
+
+            for (int i = 1; i < m; i++){
+                if (!minus.isEmpty()){
+                    minus.poll();
+                }
+            }
+        }
+
+        System.out.println(step - maxVal);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
