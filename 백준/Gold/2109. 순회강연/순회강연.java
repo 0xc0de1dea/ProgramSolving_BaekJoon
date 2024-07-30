36c1d4551cb4a241ba5bab43963d01e7817927c4
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2024-07-30 13:48:43 +0900

    [Gold III] Title: 순회강연, Time: 176 ms, Memory: 15276 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -0,0 +1,153 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
+class Lecture {
+    int d, p;
+
+    public Lecture(int d, int p){
+        this.d = d;
+        this.p = p;
+    }
+}
+
+public class Main {
+    static int[] parent;
+
+    public static int find(int x){
+        if (parent[x] == x) return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+
+        if (a != b){
+            parent[b] = a;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //System.setIn(new java.io.FileInputStream("input.in"));
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = in.nextInt();
+        PriorityQueue<Lecture> pq = new PriorityQueue<>((x, y) -> y.p - x.p);
+        parent = new int[10_002];
+
+        for (int i = 1; i <= 10_001; i++){
+            parent[i] = i;
+        }
+
+        for (int i = 1; i <= n; i++){
+            int p = in.nextInt();
+            int d = in.nextInt();
+
+            pq.add(new Lecture(d, p));
+        }
+
+        int[] ans = new int[10_001];
+
+        while (!pq.isEmpty()){
+            Lecture cur = pq.poll();
+            int d = cur.d;
+            int p = cur.p;
+
+            if (ans[d] != 0){
+                d = find(d) - 1;
+            }
+
+            if (d > 0){
+                union(d, d + 1);
+                ans[d] = p;
+            }
+        }
+
+        int max = 0;
+
+        for (int i = 1; i <= 10_000; i++){
+            max += ans[i];
+        }
+        
+        System.out.println(max);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
