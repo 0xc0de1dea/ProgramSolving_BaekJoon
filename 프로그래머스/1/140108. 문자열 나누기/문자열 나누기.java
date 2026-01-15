6cb6d71e763bea0f05ac953add47efd5c31bd79d
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2026-01-15 09:14:15 +0900

    [level 1] Title: 문자열 나누기, Time: 19.40 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -2,30 +2,33 @@ class Solution {
     public int solution(String s) {
         int cnt = 0;
 
-        while (s != ""){
-            char target = s.charAt(0);
+        while (!s.equals("")){
+            char trg = s.charAt(0);
+
             int x = 0;
             int y = 0;
+
             int idx = 0;
 
             while (true){
-                if (idx == s.length()){
+                if (idx >= s.length()){
                     cnt++;
                     s = "";
                     break;
                 }
 
-                if (s.charAt(idx++) == target) x++;
+                if (s.charAt(idx++) == trg) x++;
                 else y++;
 
                 if (x == y){
                     cnt++;
-                    
+
                     if (idx >= s.length()){
                         s = "";
                     } else {
                         s = s.substring(idx, s.length());
                     }
+
                     break;
                 }
             }
