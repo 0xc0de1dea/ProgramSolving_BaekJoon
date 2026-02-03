49f1cd61f9efd06c768d0def3fb17df706b5a4ea
코아 <131027305+0xc0de1dea@users.noreply.github.com>
2026-02-03 09:13:51 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,59 +1,113 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Stack;
- 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Written by 0xc0de1dea
+ * Email : 0xc0de1dea@gmail.com
+ */
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < T; i++) {
-			sb.append(solve(br.readLine())).append('\n');
-		}
-		
-		System.out.println(sb);
-	}
- 
-	public static String solve(String s) {
- 
-		Stack<Character> stack = new Stack<>();
- 
-		for (int i = 0; i < s.length(); i++) {
- 
-			char c = s.charAt(i);
- 
-			// 여는 괄호일 경우 스택에 넣는다.
-			if (c == '(') {
-				stack.push(c);
-			}
- 
-			// 아래는 모두 닫는 괄호 일 경우들이다.
- 
-			// 스택이 비어있는 경우. 즉, 닫는 괄호를 입력받았으나 pop할 원소가 없을 경우
-			else if (stack.empty()) {
-				return "NO";
-			}
-			// 그 외의 경우 stack 원소를 pop 한다.
-			else {
-				stack.pop();
-			}
-		}
- 
-		/*
-		 * 모든 검사 마치고 스택에 잔여 요소가 있으면 여는 괄호가 많은 경우는 "NO" 
-		 * 스택이 비어있으면 온전한 수식이므로 "YES" 이다.
-		 */
- 
-		if (stack.empty()) {
-			return "YES";
-		} 
-		else {
-			return "NO";
-		}
-	}
-}//!@#$%^&^%$
+    public static void main(String[] args) throws Exception {
+        Reader in = new Reader();
+        StringBuilder sb = new StringBuilder();
+
+        int t = in.nextInt();
+
+        while (t-- > 0){
+            char[] cArr = in.nextString().toCharArray();
+
+            Deque<Character> stack = new ArrayDeque<>();
+
+            boolean flag = true;
+
+            jump : for (int i = 0; i < cArr.length; i++){
+                if (cArr[i] == '(') stack.push('(');
+                else if (cArr[i] == ')'){
+                    if (!stack.isEmpty()){
+                        stack.pop();
+                    } else {
+                        flag = false;
+                        break jump;
+                    }
+                }
+            }
+
+            if (flag && stack.isEmpty()) sb.append("YES").append('\n');
+            else sb.append("NO").append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
+
+class Reader {
+    final int SIZE = 1 << 13;
+    byte[] buffer = new byte[SIZE];
+    int index, size;
+
+    String nextString() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        byte c;
+        while ((c = read()) < 32) { if (size < 0) return "endLine"; }
+        do sb.appendCodePoint(c);
+        while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+        return sb.toString();
+    }
+
+    char nextChar() throws Exception {
+        byte c;
+        while ((c = read()) < 32); // SPACE 분리라면 <=로, SPACE 무시라면 <로
+        return (char)c;
+    }
+    
+    int nextInt() throws Exception {
+        int n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -1; }
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    long nextLong() throws Exception {
+        long n = 0;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32);
+        if (c == 45) { c = read(); isMinus = true; }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = read()));
+        return isMinus ? ~n + 1 : n;
+    }
+
+    double nextDouble() throws Exception {
+        double n = 0, div = 1;
+        byte c;
+        boolean isMinus = false;
+        while ((c = read()) <= 32) { if (size < 0) return -12345; }
+        if (c == 45) { c = read(); isMinus = true; }
+        else if (c == 46) { c = read(); }
+        do n = (n * 10) + (c & 15);
+        while (isNumber(c = read()));
+        if (c == 46) { while (isNumber(c = read())){ n += (c - 48) / (div *= 10); }}
+        return isMinus ? -n : n;
+    }
+
+    boolean isNumber(byte c) {
+        return 47 < c && c < 58;
+    }
+
+    boolean isAlphabet(byte c){
+        return (64 < c && c < 91) || (96 < c && c < 123);
+    }
+
+    byte read() throws Exception {
+        if (index == size) {
+            size = System.in.read(buffer, index = 0, SIZE);
+            if (size < 0) buffer[0] = -1;
+        }
+        return buffer[index++];
+    }
+}
